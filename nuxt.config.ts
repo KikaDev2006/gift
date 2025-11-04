204edea154e1996b2bfb6800f3cf45ea22b98a9c
KikaDev2006 <laika9440@gmail.com>
2025-11-03 21:04:21 -0300

    feat

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,15 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  css: ['./app/assets/css/main.css'],
+  css: ["./app/assets/css/main.css"],
+
+  ssr: false, // 👈 Desactiva SSR (solo frontend, tipo SPA)
+  
+  nitro: {
+    preset: "static", // 👈 Genera una versión estática al hacer `nuxi generate`
+  },
+
   vite: {
-    plugins: [
-      tailwindcss(),
-      
-    ],
+    plugins: [tailwindcss()],
   },
 });
